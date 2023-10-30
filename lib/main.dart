ac7dd7f4c51df94cfcf70d9798c02a8ec75f058c
ttancter1303 <01689674504zx@gmail.com>
2023-10-30 16:08:53 +0700

    ttan: 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app2/quiz.dart';
 import 'package:quiz_app2/start_screen.dart';
+
 void main(){
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 69, 6, 121),
-                Color.fromARGB(255, 112, 13, 217),
-              ]
-          ),
-        ),
-        child: const StartScreen(),
-      ),
-    ),
-  ));
+  runApp(const Quiz());
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app2/start_screen.dart';
+
+class Quiz extends StatefulWidget{
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+  
+}
+class _QuizState extends State<Quiz>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 69, 6, 121),
+                  Color.fromARGB(255, 112, 13, 217),
+                ]
+            ),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    );
+  }
+  
+}
