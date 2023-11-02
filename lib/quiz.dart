1bcccd26e66964a3b1d8844772876cfc33592b77
ttancter1303 <01689674504zx@gmail.com>
2023-11-02 19:00:28 +0700

    sd

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,24 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app2/questions_screen.dart';
 import 'package:quiz_app2/start_screen.dart';
 
-class Quiz extends StatefulWidget{
+class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
   @override
   State<Quiz> createState() {
     return _QuizState();
   }
-  
 }
-class _QuizState extends State<Quiz>{
-  Widget activeScreen  = const StartScreen(switchScreen);
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
   @override
   void initState() {
     super.initState();
   }
-  void switchScreen(){
+
+  void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -30,17 +31,16 @@ class _QuizState extends State<Quiz>{
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 69, 6, 121),
-                  Color.fromARGB(255, 112, 13, 217),
-                ]
-            ),
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 69, 6, 121),
+              Color.fromARGB(255, 112, 13, 217),
+            ]),
           ),
-          child: activeScreen ,
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
   }
-  
 }
