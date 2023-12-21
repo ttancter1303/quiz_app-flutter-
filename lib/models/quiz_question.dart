f58696446796f3c6b4a469cded115fdf6fcb2d99
ttancter1303 <01689674504zx@gmail.com>
2023-12-22 00:44:49 +0700

    daily commit

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -2,4 +2,10 @@ class QuizQuestion {
   const QuizQuestion(this.text, this.answers);
   final String text;
   final List<String> answers;
+
+  List<String> shuffledAnswers(){
+    final shuffleList = List.of(answers);
+    shuffleList.shuffle();
+    return shuffleList;
+  }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -18,15 +18,24 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(currentQuestion.text),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(answerText: answer, onTap: () {});
-          })
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(currentQuestion.text,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.shuffledAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            })
+          ],
+        ),
       ),
     );
   }
